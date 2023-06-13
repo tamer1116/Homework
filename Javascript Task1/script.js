3ce88c112ba1d9ed4df8d844ed62ffb083d1c982
Your Name <tamerlan.i0203@gmail.com>
2023-06-13 15:37:35 +0400

    Example

---
+++ Javascript Task1/script.js
@@ -0,0 +1,76 @@
+
+
+
+//          BIRINCI
+// alert("'My name is Alex'");
+
+
+
+//            IKINCI
+// var number = prompt("Rəqəm daxil edin. ");
+// if (number % 2 == 0) {
+//     console.log("Rəqəm cütdür.");
+// }
+// else {
+//     console.log("Rəqəm təkdir.");
+// }
+
+
+
+//             ÜÇÜNCÜ
+// var number1 = prompt("1. Rəqəmi daxil edin. ");
+// var number2 = prompt("2. Rəqəmi daxil edin. ");
+// if (number1 > number2) {
+//     console.log(number1 + "Daha böyükdür.");
+// }
+// else if (number1 < number2) {
+//     console.log(number2 + "Daha böyükdür.");
+// }
+// else {
+//     console.log("Rəqəmlər bərabərdir.");
+// }
+
+
+
+//              DÖRDÜNCÜ
+// var number = prompt("Rəqəm daxil edin. ");
+// if (number > 0) {
+//     console.log("Müsbət rəqəm.");
+// }
+// else if (number < 0) {
+//     console.log("Mənfi rəqəm.");
+// }
+// else {
+//     console.log("0");
+// }
+
+
+
+//             BEŞİNCİ
+// var number = prompt("40-100 arası rəqəm daxil edin.")
+// if (number >= 90 && number <= 100) {
+//     console.log("S grade")
+// }
+// else if (number >= 80 && number <= 90) {
+//     console.log("A grade ")
+// }
+// else if (number >= 70 && number <= 80) {
+//     console.log("B grade ")
+// }
+// else if (number >= 60 && number <= 70) {
+//     console.log("C grade ")
+// }
+// else if (number >= 50 && number <= 60) {
+//     console.log("D grade ")
+// }
+// else if (number >= 40 && number <= 50) {
+//     console.log("E grade ")
+// }
+// else {
+//     console.log("Yanlış rəqəm. 40-100 arası rəqəm daxil edin.")
+// }
+
+
+
+
+
