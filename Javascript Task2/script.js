b4ebe285802a67f64383e1b98778a2aef10cc3fb
Your Name <tamerlan.i0203@gmail.com>
2023-06-21 12:34:06 +0400

    Example

---
+++ Javascript Task2/script.js
@@ -0,0 +1,95 @@
+// function num(number) {
+//     console.log(number ** 2)
+// }
+// num(5);
+
+
+
+
+// function celsFArh(celsius) {
+//     console.log((celsius * 9 / 5) + 32)
+// }
+// celsFArh(25)
+
+
+
+
+// function add() {
+//     let num1 = prompt("Birinci reqemi daxil edin");
+//     let num2 = prompt("Ikinci reqemi daxil edin");
+//     let total = Number(num1) + Number(num2);
+//     console.log(total);
+// }
+// add();
+
+
+
+
+
+
+//         loops
+
+
+// let test = prompt("Reqem daxil edin")
+// let num = test
+// let test2 = 0
+// for (let i = 1; i <= num; i++) {
+//     test2 += i;
+// }
+// console.log(test2)
+
+
+
+
+// let num = 5
+// let test = 1
+// for (let i = 1; i <= num; i++) {
+//     test *= i;
+// }
+// console.log(test);
+
+
+
+
+
+// let test = prompt("Reqem daxil edin")
+// for (let i = 1; i <= test; i++) {
+//     console.log("Hello World!")
+// }
+
+
+
+
+// let test = prompt("Reqem daxil edin")
+// let test2 = 0;
+// for (let i = 1; i <= test; i++) {
+//     if (i % 3 == 0)
+//         test2 += i;
+
+// }
+// console.log(test2)
+
+
+
+
+// let test = prompt("Reqem daxil edin")
+// for (let i = 1; i <= test; i++) {
+//     if (i % 3 == 0)
+//         console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
